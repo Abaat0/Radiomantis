3088d85e507b2586e6f038c944a3aa2577320854
Albert <github@sl.tuneski.net>
2026-03-07 17:28:13 +0100

    fixed js to accomodate actual player

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const btn = document.querySelector(".button");
-    
-    btn.addEventListener("click", function() {
-        btn.classList.toggle("paused");
+document.addEventListener('DOMContentLoaded', () => {
+    const playButton = document.querySelector('.button');
+    const audioStream = document.getElementById('radio-stream');
+
+    playButton.addEventListener('click', () => {
+        if (audioStream.paused) {
+            audioStream.play();
+            playButton.classList.add('paused'); 
+        } else {
+            audioStream.pause();
+            playButton.classList.remove('paused');
+            
+            // Reset the source so when they hit play again, it catches up to live
+            audioStream.src = audioStream.src; 
+        }
     });
 });
